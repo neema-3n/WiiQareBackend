2000325d84203f29eea0819e265e87ce3ad7a18d
Mucyo Fred <fred.mucyo.io@gmail.com>
2023-05-16 23:56:58 +0200

    bg - save image to object storage only if available

--- src/modules/object-storage/object-storage.service.ts
+++ src/modules/object-storage/object-storage.service.ts
@@ -5,13 +5,18 @@ import { MinioService } from 'nestjs-minio-client';
 @Injectable()
 export class ObjectStorageService {
   bucketName = 'logos';
-  constructor(private readonly minioService: MinioService) { }
 
-  async saveObject(file: Express.Multer.File): Promise<UploadedObjectInfo> {
-    return this.minioService.client.putObject(
-      this.bucketName,
-      file.originalname,
-      file.buffer,
-    );
+  constructor(private readonly minioService: MinioService) {}
+
+  async saveObject(
+    file: Express.Multer.File,
+  ): Promise<UploadedObjectInfo | undefined> {
+    if (file)
+      return this.minioService.client.putObject(
+        this.bucketName,
+        file.originalname,
+        file.buffer,
+      );
+    return;
   }
 }
