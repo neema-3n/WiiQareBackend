a8d0c45b048b0614321b4dd547b6a95fbe7777f2
miller <125817291+millerIO@users.noreply.github.com>
2023-06-02 00:00:18 +0200

    Fix/mail templates (#45)
    * ft - adds reedem voucher and statistics endpoint
    
    * ch - fix issue with mail templates

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -1,7 +1,7 @@
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { Module } from '@nestjs/common';
-import { join } from 'path';
+import path from 'path';
 import { AppConfigService } from 'src/config/app-config.service';
 import { MailService } from './mail.service';
 
@@ -21,7 +21,7 @@ import { MailService } from './mail.service';
           from: '"No Reply" <wiiqare@google.com>',
         },
         template: {
-          dir: join(__dirname, 'templates'),
+          dir: path.resolve(__dirname, '../../../templates'),
           adapter: new HandlebarsAdapter(), // or new PugAdapter() or new EjsAdapter()
           options: {
             strict: true,
