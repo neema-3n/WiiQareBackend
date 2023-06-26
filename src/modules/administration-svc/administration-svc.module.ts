9564e6bb82e6d18383bf82ffb6ca1f90b7be7eaf
Nana Emmanuel <nana.emmanuel131@gmail.com>
2023-06-26 13:40:03 +0100

    Feat : Add admin registration

--- src/modules/administration-svc/administration-svc.module.ts
+++ src/modules/administration-svc/administration-svc.module.ts
@@ -6,12 +6,10 @@ import { Payer } from '../payer-svc/entities/payer.entity';
 import { Transaction } from '../smart-contract/entities/transaction.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../session/entities/user.entity';
-import { PayerService } from './payers/payers.service';
-import { PayerController } from './payers/payers.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payer, Transaction, User])],
-  providers: [AdminService, PayerService],
-  controllers: [AdminController, PayerController],
+  providers: [AdminService,],
+  controllers: [AdminController, ],
 })
 export class AdministrationSvcModule {}
