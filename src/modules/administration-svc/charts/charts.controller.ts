037d11c6f535c32b12f025953c5f22557428c6a0
Nana Emmanuel <nana.emmanuel131@gmail.com>
2023-07-11 14:21:02 +0100

    ft(charts) : protect /admin/charts route

--- src/modules/administration-svc/charts/charts.controller.ts
+++ src/modules/administration-svc/charts/charts.controller.ts
@@ -3,7 +3,7 @@ import { ChartsService } from './charts.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UserRole } from 'src/common/constants/enums';
 import { Roles } from 'src/common/decorators/user-role.decorator';
-import { Public } from 'src/common/decorators/public.decorator';
+// import { Public } from 'src/common/decorators/public.decorator';
 import { ChartsDTO } from './dto/chart.dto';
 
 @ApiTags('admin/charts')
@@ -12,8 +12,8 @@ export class ChartsController {
   constructor(private chartService: ChartsService) {}
 
   @Get()
-  //   @Roles(UserRole.WIIQARE_ADMIN)
-  @Public()
+  @Roles(UserRole.WIIQARE_ADMIN)
+  //   @Public()
   @ApiOperation({
     summary:
       'API endpoint to get informations used for charts on the homepage of dashboard',
