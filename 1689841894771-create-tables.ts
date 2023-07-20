5d847f2765ec06ad6f2ee8846af9e19cc4a337a9
WiiQare <wiiqare@gmail.com>
2023-07-20 10:45:09 +0100

    creattable

---
+++ 1689841894771-create-tables.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class createTables1689841894771 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
