493e12d5f0e52db868f326f5957233ad9783fe95
abhishandy <abhishek.sandy@gmail.com>
2023-08-23 22:48:36 -0400

    chore: formatting

--- 1689841894771-create-tables.ts
+++ 1689841894771-create-tables.ts
@@ -1,11 +1,7 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class createTables1689841894771 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
