43d311848ff0ba2747cf9ab6735ee2f06ff37f39
abhishandy <abhishek.sandy@gmail.com>
2023-08-24 21:32:45 -0400

    test: admin charts service

--- src/modules/administration-svc/charts/charts.service.test.ts
+++ src/modules/administration-svc/charts/charts.service.test.ts
@@ -1,10 +1,48 @@
 import { DataSource } from 'typeorm';
 import { ChartsService } from './charts.service';
+import * as PayersChartInfoQueryBuilder from './querybuilders/payersChart.qb';
+import * as BeneficiariesChartInfoQueryBuilder from './querybuilders/beneficiariesChart.qb';
 
 describe('ChartsService', () => {
-  it('should be defined', () => {
+  let service: ChartsService;
+  beforeEach(() => {
+    jest.resetAllMocks();
     const dataSource: DataSource = {} as DataSource;
-    const service = new ChartsService(dataSource);
+    service = new ChartsService(dataSource);
+  });
+
+  it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findPayersChartInfo', () => {
+    jest
+      .spyOn(PayersChartInfoQueryBuilder, 'getPayersChartInfoQueryBuilder')
+      .mockReturnValue({
+        orderBy: jest.fn().mockReturnThis(),
+        addOrderBy: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockResolvedValue([]),
+      } as any);
+
+    const info = service.findPayersChartInfo(1);
+    expect(info).toBeDefined();
+  });
+
+  it('findBeneficiariesChartInfo', () => {
+    jest
+      .spyOn(
+        BeneficiariesChartInfoQueryBuilder,
+        'getBeneficiariesChartInfoQueryBuilder',
+      )
+      .mockReturnValue({
+        orderBy: jest.fn().mockReturnThis(),
+        addOrderBy: jest.fn().mockReturnThis(),
+        limit: jest.fn().mockReturnThis(),
+        getRawMany: jest.fn().mockResolvedValue([]),
+      } as any);
+
+    const info = service.findBeneficiariesChartInfo(1);
+    expect(info).toBeDefined();
   });
 });
