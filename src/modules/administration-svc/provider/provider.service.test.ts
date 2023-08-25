9fe268d8c5b8f7d302ab52b136f7bd3c7deaf108
abhishandy <abhishek.sandy@gmail.com>
2023-08-24 22:34:05 -0400

    test: admin provider service

--- src/modules/administration-svc/provider/provider.service.test.ts
+++ src/modules/administration-svc/provider/provider.service.test.ts
@@ -1,10 +1,95 @@
 import { DataSource } from 'typeorm';
 import { ProviderService } from './provider.service';
+import * as ProviderSummary from './querybuilders/getProviderSummary.qb';
 
 describe('ProviderService', () => {
-  it('should be defined', () => {
+  let service: ProviderService;
+
+  beforeEach(async () => {
     const dataSource: DataSource = {} as DataSource;
-    const service = new ProviderService(dataSource);
+    service = new ProviderService(dataSource);
+  });
+
+  it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return provider summary', async () => {
+    jest
+      .spyOn(
+        ProviderSummary,
+        'getTotalBeneficiaryTransactionMadeWithinOneWeekQueryBuilder',
+      )
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(
+        ProviderSummary,
+        'getTotalBeneficiaryTransactionMadeWithinOneMonthQueryBuilder',
+      )
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(
+        ProviderSummary,
+        'getTotalBeneficiaryTransactionMadeWithinThreeMonthsQueryBuilder',
+      )
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(
+        ProviderSummary,
+        'getTotalBeneficiaryToProviderTransactionQueryBuilder',
+      )
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(ProviderSummary, 'getTotalNumberOfUniqueBeneficiaryQueryBuilder')
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(ProviderSummary, 'getAllUnclaimedVouchersQueryBuilder')
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(ProviderSummary, 'getAllClaimedVouchersQueryBuilder')
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(ProviderSummary, 'getAllRedeemedVouchersQueryBuilder')
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(ProviderSummary, 'getNumberOfRegisteredProvidersQueryBuilder')
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+
+    jest
+      .spyOn(
+        ProviderSummary,
+        'getTotalBeneficiaryTransactionMadeWithinSixMonthsQueryBuilder',
+      )
+      .mockReturnValue({
+        getRawOne: jest.fn().mockResolvedValue({}),
+      } as any);
+    const summary = await service.getSummary();
+    expect(summary).toBeDefined();
   });
 });
